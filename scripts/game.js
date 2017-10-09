6766e9f5e51fed124962e3b9f3e1a4fefd8759b6
silviudev <silviudev@yahoo.com>
2017-10-08 21:29:30 -0700

    Added reset button functionality and text color change on victory

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,14 @@
 "use strict";
 
-var buttonArray = Array.from(document.getElementsByTagName("button"));
+var gameSquareArray = Array.from(document.getElementsByClassName("gameSquare"));
 var turnDisplay = document.getElementById("turnDisplay");
+var resetButton = document.getElementById("restart");
+var instructionDisplay = document.getElementById("instructions");
 var turn = "x";
 
-buttonArray.forEach(function(item){
+resetButton.addEventListener("click", resetGame);
+
+gameSquareArray.forEach(function(item){
 	item.addEventListener("click", handleButtonClick);
 });
 
@@ -25,14 +29,23 @@ function handleButtonClick(){
 		turnDisplay.innerHTML = "X's turn";
 		turn = "x";
 	}
-	
-	if(checkGameOverConditions() === "x"){
-		turnDisplay.innerHTML = "GAME OVER - X WINS!";
-	}else if(checkGameOverConditions() === "o"){
-		turnDisplay.innerHTML = "GAME OVER - O WINS!";
-	}else if(checkGameOverConditions() === "tie"){
-		turnDisplay.innerHTML = "GAME OVER - IT'S A TIE!";
+
+	if(checkGameOverConditions() !== ""){
+		resetButton.style.visibility = "visible";
+		resetButton.style.margin = "5px auto 5px auto";
+		instructionDisplay.innerHTML = "Click the button below to reset";
+		if(checkGameOverConditions() === "x"){
+			turnDisplay.innerHTML = "GAME OVER - X WINS!";
+			markWinnerTextRed("x");
+		}else if(checkGameOverConditions() === "o"){
+			turnDisplay.innerHTML = "GAME OVER - O WINS!";
+			markWinnerTextRed("o");
+		}else if(checkGameOverConditions() === "tie"){
+			turnDisplay.innerHTML = "GAME OVER - IT'S A TIE!";
+			markWinnerTextRed("tie");
+		}
 	}
+
 
 }
 
@@ -41,15 +54,15 @@ checkGameOverConditions() checks the board and returns "x" if X won, "o" if O wo
 "tie" if all squares are filled but no one won, or an empty string if the game isn't over yet.
 */
 function checkGameOverConditions(){
-	var b0h = buttonArray[0].innerHTML.toLowerCase();
-	var b1h = buttonArray[1].innerHTML.toLowerCase();
-	var b2h = buttonArray[2].innerHTML.toLowerCase();
-	var b3h = buttonArray[3].innerHTML.toLowerCase();
-	var b4h = buttonArray[4].innerHTML.toLowerCase();
-	var b5h = buttonArray[5].innerHTML.toLowerCase();
-	var b6h = buttonArray[6].innerHTML.toLowerCase();
-	var b7h = buttonArray[7].innerHTML.toLowerCase();
-	var b8h = buttonArray[8].innerHTML.toLowerCase();
+	var b0h = gameSquareArray[0].innerHTML.toLowerCase();
+	var b1h = gameSquareArray[1].innerHTML.toLowerCase();
+	var b2h = gameSquareArray[2].innerHTML.toLowerCase();
+	var b3h = gameSquareArray[3].innerHTML.toLowerCase();
+	var b4h = gameSquareArray[4].innerHTML.toLowerCase();
+	var b5h = gameSquareArray[5].innerHTML.toLowerCase();
+	var b6h = gameSquareArray[6].innerHTML.toLowerCase();
+	var b7h = gameSquareArray[7].innerHTML.toLowerCase();
+	var b8h = gameSquareArray[8].innerHTML.toLowerCase();
 	
 	if((b0h === "x" && b1h === "x" && b2h === "x") || 
 		 (b3h === "x" && b4h === "x" && b5h === "x") ||
@@ -80,7 +93,37 @@ allSquaresFull() checks if all the squares of the game are full, returns true if
 false if not.
 */
 function allSquaresFull(){
-	return buttonArray.every(function(item){
+	return gameSquareArray.every(function(item){
 		return item.touched;
+	});
+}
+
+/*
+resetGame() resets the game by resetting game variables such as the turn and
+clearing all game squares, then hides the reset button. Return type void.
+*/
+function resetGame(){
+	turn = "x";
+	gameSquareArray.forEach(function(item){
+		item.innerHTML = "";
+		item.touched = false;
+		item.style.color = "black";
+	});
+	resetButton.style.visibility = "hidden";
+	resetButton.style.margin = "-5px";
+	turnDisplay.innerHTML = "X's turn";
+	instructionDisplay.innerHTML = "Click a square to mark it";
+}
+/*
+markWinnerTextRed() changes the color of the winner's text to red for the user to more easily
+see the victory. Return type void.
+*/
+function markWinnerTextRed(winner){
+	gameSquareArray.forEach(function(item){
+		if(item.innerHTML.toLowerCase() === winner){
+			item.style.color = "red";
+		}else if(winner.toLowerCase() === "tie"){
+			item.style.color = "gray";
+		}
 	});
 }
